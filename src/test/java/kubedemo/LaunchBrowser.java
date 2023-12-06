74c768c160f4cf37132e34248128289bcf342b2f
Saidinesh Narisetti <saidinesh.narisetti@thoughtworks.com>
2023-12-06 19:47:27 +0530

    Updating selenium HUB URL

--- src/test/java/kubedemo/LaunchBrowser.java
+++ src/test/java/kubedemo/LaunchBrowser.java
@@ -9,38 +9,35 @@ import org.junit.AfterClass;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-
 public class LaunchBrowser {
-	public static RemoteWebDriver driver=null;
-	@Parameters({ "browser", "seleniumHubUrl" }) // Used TestNg parameterization concept 
-	@BeforeClass 
-	public void opendriver(String browser, String seleniumHubUrl)  throws MalformedURLException { 
-		System.out.println("browser passed as :- " + browser);
-		System.out.println("Selenium Hub URL: :"+ seleniumHubUrl);
-		if(browser.equalsIgnoreCase("Chrome")) {
-			ChromeOptions options = new ChromeOptions();
-			//URL u=new URL("http://127.0.0.1:53908/wd/hub");
-			URL u=new URL("seleniumHubUrl");
-			driver = new RemoteWebDriver (u,options);	
-		}
-		if(browser.equalsIgnoreCase("firefox")) {
-			FirefoxOptions foptions =new FirefoxOptions();
-			//URL u=new URL("http://127.0.0.1:53908/wd/hub");
-			URL u=new URL("seleniumHubUrl");
-			driver = new RemoteWebDriver(u,foptions);	
-			}
-		if(browser.equalsIgnoreCase("Edge")) {
-			EdgeOptions Eoptions =new EdgeOptions();
-			//URL u=new URL("http://127.0.0.1:53908/wd/hub");
-			URL u=new URL("seleniumHubUrl");
-			driver = new RemoteWebDriver(u,Eoptions);	
-			}
-	} 
-  @AfterClass
-  public void terminateBrowser(){
-      driver.close();			///Closing browser
-	}
+
+    public static RemoteWebDriver driver = null;
+
+    @Parameters({ "browser", "seleniumHubUrl" })
+    @BeforeClass
+    public void openDriver(String browser, String seleniumHubUrl) throws MalformedURLException {
+        System.out.println("Browser passed as: " + browser);
+        System.out.println("Selenium Hub URL: " + seleniumHubUrl);
+
+        if (browser.equalsIgnoreCase("Chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            URL u = new URL(seleniumHubUrl);
+            driver = new RemoteWebDriver(u, options);
+        } else if (browser.equalsIgnoreCase("Firefox")) {
+            FirefoxOptions foptions = new FirefoxOptions();
+            URL u = new URL(seleniumHubUrl);
+            driver = new RemoteWebDriver(u, foptions);
+        } else if (browser.equalsIgnoreCase("Edge")) {
+            EdgeOptions Eoptions = new EdgeOptions();
+            URL u = new URL(seleniumHubUrl);
+            driver = new RemoteWebDriver(u, Eoptions);
+        }
+    }
+
+    @AfterClass
+    public void terminateBrowser() {
+        driver.quit(); // Quitting the browser
+    }
 }
